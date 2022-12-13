4acee5f82f6432d0886e1db6a4de80e1d490aaf7
Nate Sesti <sestinj@gmail.com>
2022-12-13 11:08:07 -0500

    llm.py

---
+++ llm.py
@@ -0,0 +1,43 @@
+import openai
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+openai.api_key = os.environ.get("OPENAI_API_KEY")
+
+class LLM:
+    def complete(self, prompt: str,
+            temp=0.5,
+            max_tokens=100,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0,
+            stop=[]):
+        """Return the completion of the text with the given temperature."""
+        raise NotImplementedError
+    
+    def fine_tune(self, pairs: list):
+        """Fine tune the model on the given prompt/completion pairs."""
+        raise NotImplementedError
+
+class OpenAI(LLM):
+    def __init__(self, engine: str = "davinci"):
+        self.engine = engine
+        
+    def complete(self, prompt: str,
+            temp=0.5,
+            max_tokens=100,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0,
+            stop=[]):
+        return self.openai.Completion.create(
+            engine=self.engine,
+            prompt=prompt,
+            temperature=temp,
+            max_tokens=max_tokens,
+            top_p=top_p,
+            frequency_penalty=frequency_penalty,
+            presence_penalty=presence_penalty,
+            stop=stop
+        ).choices[0].text
