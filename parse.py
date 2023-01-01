aa2d9e5a6f7d9cc4aed88b85e155e79f0f97ce83
Nate Sesti <sestinj@gmail.com>
2023-01-01 10:55:59 -0500

    function signature

--- parse.py
+++ parse.py
@@ -1,19 +1,37 @@
 import ast
+import docstring_parser
 
-def arg_signature(arg: ast.arg) -> str:
-    """Get the signature of an argument."""
-    if arg.annotation is None:
-        return arg.arg
-    else:
-        return f"{arg.arg}: {arg.annotation.id}"
 
 def fn_signature(fn: ast.FunctionDef) -> str:
-    """Get the signature of a function."""
+    """Get the signature of a function. Check for literal type annotations first, then docstring annotations."""
+
+    docstring = ast.get_docstring(fn)
+    parsed_ds = docstring_parser.parse(ast.get_docstring(fn))
+    ds_args = {}
+    ds_return = None
+    for meta in parsed_ds.meta:
+        if isinstance(meta, docstring_parser.common.DocstringReturns):
+            ds_return = meta.type_name
+        elif isinstance(meta, docstring_parser.common.DocstringParam):
+            ds_args[meta.arg_name] = meta.type_name
+        else:
+            print("Unknown meta type: ", type(meta))
+    
+    def arg_signature(arg: ast.arg) -> str:
+        """Get the signature of an argument."""
+        if arg.annotation is not None:
+            return f"{arg.arg}: {arg.annotation.id}"
+        elif arg.arg in ds_args:
+            return f"{arg.arg}: {ds_args[arg.arg]}"
+        else:
+            return arg.arg
+    
     sig = f"fn {fn.name}({', '.join([arg_signature(arg) for arg in fn.args.args])})"
     if fn.returns is not None:
         sig += f" -> {fn.returns.id}"
+    elif ds_return is not None:
+        sig += f" -> {ds_return}"
 
-    docstring = ast.get_docstring(fn)
     if docstring is not None:
         sig = f'"""{docstring}"""\n' + sig
     
