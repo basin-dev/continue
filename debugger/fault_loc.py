580fcee3681f60e20fbe77ff4697147ff99e9bcb
tydunn <t.dunn@rasa.com>
2023-01-16 15:05:23 +0100

    all function calls and basic SBFL approach

--- debugger/fault_loc.py
+++ debugger/fault_loc.py
@@ -45,7 +45,7 @@ def edit_context_ast(frame: Dict, replacement: Callable[[str], str]) -> ast.AST 
         for child in ast.iter_child_nodes(parent):
             if is_valid_context(child, lineno):
                 if len(list(filter(lambda x: is_valid_context(x, lineno), child.body))) > 0:
-                    # More specific context can be found inthe body of this function
+                    # More specific context can be found in the body of this function
                     continue
 
                 str_node = ast.unparse(child)
@@ -55,3 +55,71 @@ def edit_context_ast(frame: Dict, replacement: Callable[[str], str]) -> ast.AST 
             i += 1
 
     return None
+
+def get_coverage_data(filename):
+    """Get the coverage data for a file."""
+    import coverage
+
+    cov = coverage.Coverage()
+
+    cov.start()
+
+    import script
+
+    cov.stop()
+
+    return cov.get_data()
+
+def get_program_structure(filename):
+    """Get the program structure for a file."""
+
+    with open(filename, "r") as f:
+        code = f.read()
+
+    tree = ast.parse(code)
+
+    return ast.walk(tree)
+
+def fault_localization(coverage_data, program_structure):
+    """Calculate the suspiciousness of each statement in the program."""
+    
+    suspiciousness = {}
+    
+    for statement in program_structure:
+        executed = coverage_data[statement]
+        
+        total = sum(coverage_data.values())
+        
+        suspiciousness[statement] = 1 - (executed / total)
+    
+    return suspiciousness
+
+def find_suspcious_lines():
+    """Find the suspicious lines in a file."""
+    coverage_data = get_coverage_data("script.py")
+    program_structure = get_program_structure("script.py")
+    suspiciousness = fault_localization(coverage_data, program_structure)
+
+def find_function_calls(node, function_name, function_calls=[]):
+    """Find all function calls to a function in an AST."""
+    if isinstance(node, ast.Call):
+
+        if hasattr(node.func, 'id') and node.func.id == function_name:
+
+            function_calls.append((node.lineno, ast.get_source_segment(node)))
+
+    for child in ast.iter_child_nodes(node):
+
+        find_function_calls(child, function_name, function_calls)
+
+    return function_calls
+
+def get_function_calls(file_path, function_name):
+    """Get all function calls to a function in a file."""
+    with open(file_path, 'r') as file:
+
+        root = ast.parse(file.read())
+
+        function_calls = find_function_calls(root, function_name)
+
+        return function_calls
