318caddac99f85c2bb8c95f06a97cb659424f807
Nate Sesti <sestinj@gmail.com>
2023-01-29 17:01:44 -0500

    Telemetry with segment!

--- autodebug/src/extension.ts
+++ autodebug/src/extension.ts
@@ -2,8 +2,11 @@ import * as vscode from "vscode";
 import { registerAllCommands } from "./commands";
 import DebugViewProvider from "./DebugViewProvider";
 import { MyCodeLensProvider } from "./languageServer";
+import { sendTelemetryEvent, TelemetryEvent } from "./telemetry";
 
 export function activate(context: vscode.ExtensionContext) {
+  sendTelemetryEvent(TelemetryEvent.ExtensionActivated);
+
   const debugViewProvider = new DebugViewProvider(context.extensionUri);
   const codeLensProvider = new MyCodeLensProvider();
 
