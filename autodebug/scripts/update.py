775d88b7e281624be1add44a835e8eda3cfe9cd8
tydunn <t.dunn@rasa.com>
2023-02-10 13:57:08 +0100

    update working

--- autodebug/scripts/update.py
+++ autodebug/scripts/update.py
@@ -1,79 +1,61 @@
 import faiss
 import json
 import os
-import pathspec
 import subprocess
 
 from gpt_index import GPTSimpleVectorIndex, SimpleDirectoryReader, Document, GPTFaissIndex
 from typing import List, Generator, Tuple
 
-
-DEFAULT_GIT_IGNORE_PATTERNS = [
-    "**/.env",
-    "**/env",
-    "**/venv",
-    "**/node_modules",
-    "**/__pycache__",
-    "**/dist",
-    "**/build",
-    "**/.pytest_cache",
-    "**/.mypy_cache",
-    "**/.coverage",
-    "**/.DS_Store",
-    "**/coverage.xml",
-    "**/bin/**",
-    "**/opt/**",
-    "**/env/**"
-    "autodebug/scripts/data/**"
+FILE_TYPES_TO_IGNORE = [
+    '.pyc',
+    '.png',
+    '.jpg',
+    '.jpeg',
+    '.gif',
+    '.pdf',
+    '.svg',
+    '.ico'
 ]
 
+def further_filter(files: List[str], root_dir: str):
+    """Further filter files before indexing."""
+    for file in files:
+        if file.endswith(tuple(FILE_TYPES_TO_IGNORE)) or file.startswith('.git') or file.startswith('archive'):
+            continue
+        yield root_dir + "/" + file
 
-def upward_search_in_filetree(search_for: str, start_path: str=".") -> List[str]:
-    """Find all files of the name search_for in parent directories of the given path."""
-    found = []
-    while True:
-        # Check whether there is a file named search_for here
-        target_file_path = os.path.join(start_path, search_for)
-        if os.path.exists(target_file_path):
-            found.append(target_file_path)
-        
-        parent_dir = os.path.abspath(os.path.join(start_path, os.pardir))
-        if parent_dir == start_path:
-            # Reached root
-            break
+def get_git_root_dir(path: str):
+    """Get the root directory of a Git repository."""
+    try:
+        return subprocess.check_output(['git', 'rev-parse', '--show-toplevel'], cwd=path).strip().decode()
+    except subprocess.CalledProcessError:
+        return None
 
-        # Move up one directory
-        start_path = parent_dir
+def get_git_ignored_files(root_dir: str):
+    """Get the list of ignored files in a Git repository."""
+    try:
+        output = subprocess.check_output(['git', 'ls-files', '--ignored', '--others', '--exclude-standard'], cwd=root_dir).strip().decode()
+        return output.split('\n')
+    except subprocess.CalledProcessError:
+        return []
 
-    return found
+def get_all_files(root_dir: str):
+    """Get a list of all files in a directory."""
+    for dir_path, _, file_names in os.walk(root_dir):
+        for file_name in file_names:
+            yield os.path.join(os.path.relpath(dir_path, root_dir), file_name)
 
-
-def build_gitignore_spec(gitignore_paths: List[str]=None, custom_match_patterns: List[str]=[]) -> pathspec.PathSpec:
-    """Build a pathspec.PathSpec from a given list of .gitignore files."""
-    if gitignore_paths is None:
-        gitignore_paths = upward_search_in_filetree(".gitignore", ".")
-
-    # Acculumate lines from all .gitignore files
-    lines = set(custom_match_patterns)
-    for gitignore_path in gitignore_paths:
-        try:
-            with open(gitignore_path, "r") as f:
-                lines.update(f.readlines())
-        except BaseException:
-            # Don't throw if the file doesn't exist
-            pass
-
-    print(lines)
-
-    return pathspec.PathSpec.from_lines(pathspec.patterns.GitWildMatchPattern, lines)
-
+def get_input_files(root_dir: str):
+    """Get a list of all files in a Git repository that are not ignored."""
+    ignored_files = set(get_git_ignored_files(root_dir))
+    all_files = set(get_all_files(root_dir))
+    nonignored_files = all_files - ignored_files
+    return further_filter(nonignored_files, root_dir)
 
 def load_gpt_index_documents(root: str) -> List[Document]:
     """Loads a list of GPTIndex Documents, respecting .gitignore files."""
-    # Build a pathspec.PathSpec from the .gitignore files
-    gitignore_spec = build_gitignore_spec()
-    # Walk the root directory to get a list of all non-ignored files
-    input_files = gitignore_spec.match_tree_files(root)
+    # Get input files
+    input_files = get_input_files(root)
     # Use SimpleDirectoryReader to load the files into Documents
     return SimpleDirectoryReader(root, input_files=input_files).load_data()
 
@@ -99,13 +81,11 @@ def create_codebase_index():
     d = 1536 # Dimension of text-ada-embedding-002
     faiss_index = faiss.IndexFlatL2(d)
     documents = load_gpt_index_documents(get_git_root_dir())
-    print(documents)
-    index = GPTFaissIndex(documents, faiss_index=faiss_index)
+    index = GPTSimpleVectorIndex(documents) # , faiss_index=faiss_index)
     index.save_to_disk(f"{index_dir_for(branch)}/index.json")
     with open(f"{index_dir_for(branch)}/metadata.json", "w") as f:
-        f.write(json.dumps({"commit": get_current_commit()}))
+        json.dump({"commit": get_current_commit()}, f, indent=4)
     print("Codebase index created")
-
 
 def get_modified_deleted_files() -> Tuple[List[str], List[str]]:
     """Get a list of all files that have been modified since the last commit."""
@@ -114,7 +94,7 @@ def get_modified_deleted_files() -> Tuple[List[str], List[str]]:
 
     metadata = f"{index_dir_for(branch)}/metadata.json"
     with open(metadata, "r") as f:
-        previous_commit = f.read()["commit"]
+        previous_commit = json.load(f)["commit"]
 
     modified_deleted_files = subprocess.check_output(["git", "diff", "--name-only", previous_commit, current_commit]).decode("utf-8").strip()
     modified_deleted_files = modified_deleted_files.split("\n")
@@ -124,31 +104,27 @@ def get_modified_deleted_files() -> Tuple[List[str], List[str]]:
     modified_files = [f for f in modified_deleted_files if os.path.exists(f)]
 
     with open(metadata, "w") as f:
-        f.write({"commit": current_commit})
+        json.dump({"commit": get_current_commit()}, f, indent=4)
 
-    return modified_files, deleted_files
+    print(f"Modified files: {modified_files}")
+    print(f"Deleted files: {deleted_files}")
 
+    return further_filter(modified_files, index_dir_for(branch)), further_filter(deleted_files, index_dir_for(branch))
 
 def update_codebase_index():
     """Update the index with a list of files."""
     branch = get_current_branch()
-    gitignore_spec = build_gitignore_spec(custom_match_patterns=DEFAULT_GIT_IGNORE_PATTERNS)
 
     if not os.path.exists(index_dir_for(branch)):
         create_codebase_index()
     else:
-        dir = index_dir_for(branch)
-        index = GPTFaissIndex.load_from_disk(f"{dir}/index.json")
+        index = GPTSimpleVectorIndex.load_from_disk(f"{index_dir_for(branch)}/index.json")
         modified_files, deleted_files = get_modified_deleted_files()
         for file in modified_files:
-            if not gitignore_spec.match_file(file):
-                index.update(file)
-            else:
-                print(f"Skipping {file} because it is ignored by .gitignore")
+            index.update(file)
         for file in deleted_files:
             index.delete(file)
         print("Codebase index updated")
-
 
 if __name__ == "__main__":
     """python3 update.py"""
