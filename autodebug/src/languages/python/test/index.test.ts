043a6c0511f832a5add597e00c637a4015d9da76
tydunn <t.dunn@rasa.com>
2023-02-16 12:49:41 +0100

    fixing test example

--- autodebug/src/languages/python/test/index.test.ts
+++ autodebug/src/languages/python/test/index.test.ts
@@ -6,12 +6,13 @@ describe("Python Test Suite", () => {
     expect(parseFirstStacktrace("")).toBeUndefined();
   });
 
-  let onboarding_trace = `Traceback (most recent call last):
+  let onboarding_trace = 
+`Traceback (most recent call last):
   File "/Users/ty/.vscode/extensions/basin.autodebug-0.0.1/examples/python/main.py", line 10, in <module>
     sum(first, second)
   File "/Users/ty/.vscode/extensions/basin.autodebug-0.0.1/examples/python/sum.py", line 2, in sum
     return a + b
-  TypeError: unsupported operand type(s) for +: 'int' and 'str'`
+TypeError: unsupported operand type(s) for +: 'int' and 'str'`;
 
   test("", () => {
     expect(parseFirstStacktrace(onboarding_trace)).toBe(onboarding_trace);
