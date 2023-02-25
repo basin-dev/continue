ab1140d10d5c81fc76ba1f19c621aa6c7b717582
Nate Sesti <sestinj@gmail.com>
2023-02-24 22:25:50 -0500

    React with Vite setup and working

---
+++ extension/react-app/vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    // Change the output .js filename to not include a hash
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`,
+      },
+    },
+  },
+});
--- extension/src/debugPanel.ts
+++ extension/src/debugPanel.ts
@@ -22,11 +22,11 @@ export function setupDebugPanel(panel: vscode.WebviewPanel): string {
 
   let extensionUri = getExtensionUri();
   const scriptUri = debugPanelWebview.asWebviewUri(
-    vscode.Uri.joinPath(extensionUri, "media", "debugPanel.js")
+    vscode.Uri.joinPath(extensionUri, "react-app/dist/assets/index.js")
   );
 
   const styleMainUri = debugPanelWebview.asWebviewUri(
-    vscode.Uri.joinPath(extensionUri, "media", "main.css")
+    vscode.Uri.joinPath(extensionUri, "react-app/dist/assets/index.css")
   );
 
   const highlightJsUri = debugPanelWebview.asWebviewUri(
@@ -175,59 +175,18 @@ export function setupDebugPanel(panel: vscode.WebviewPanel): string {
 
   return `<!DOCTYPE html>
     <html lang="en">
-    <head>
+      <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="${styleMainUri}" rel="stylesheet">
-
+        
         <link href="${highlightJsStylesUri}" rel="stylesheet">
         <script src="${highlightJsUri}"></script>
-                
+        
         <title>AutoDebug</title>
       </head>
       <body>
-        <div class="gradient">
-          <div class="container">
-            <div class="tabContainer">
-              <div class="tabBar">
-                <div class="tab selectedTab">Debug Panel</div>
-              </div>
-              <div class="contentContainer">
-                <h1>Debug Panel</h1>
-            
-                <h3>Code Sections</h3>
-                <div class="multiselectContainer"></div>
-                
-                <h3>Bug Description</h3>
-                <textarea id="bugDescription" name="bugDescription" class="bugDescription" rows="4" cols="50" placeholder="Describe your bug..."></textarea>
-                
-                <h3>Stack Trace</h3>
-                <textarea id="traceback" class="traceback" name="traceback" rows="4" cols="50" placeholder="Paste stack trace here"></textarea>
-                
-                <select hidden id="relevantVars" class="relevantVars" name="relevantVars"></select>
-                
-                <div class="buttonDiv">
-                  <button class="explainCodeButton">Explain Code</button>
-                  <button class="listTenThingsButton">Generate Ideas</button>
-                  <button disabled class="makeEditButton">Suggest Fix</button>
-                  <button disabled class="generateUnitTestButton">Create Test</button>
-                </div>
-                <div class="loader makeEditLoader" hidden></div>
-                
-                <pre class="fixSuggestion answer" hidden></pre>
-    
-                <br></br>
-              </div>
-              <div class="contentContainer" hidden>
-                <h3>Additional Context</h3>
-                <textarea rows="8" placeholder="Copy and paste information related to the bug from GitHub Issues, Slack threads, or other notes here." class="additionalContextTextarea"></textarea>
-                <br></br>
-              </div>
-            </div>
-
-          </div>
-        </div>
-
+        <div id="root"></div>
         <script nonce="${nonce}" src="${scriptUri}"></script>
       </body>
     </html>`;
